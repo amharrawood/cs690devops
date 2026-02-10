f1272cb75e42f87e9d1ef9fe8d91383a9e19ebed
amharrawood <120509907+amharrawood@users.noreply.github.com>
2026-02-10 02:27:19 +0000

    Update code

--- FactorialLibrary/FactorialService.cs
+++ FactorialLibrary/FactorialService.cs
@@ -4,9 +4,9 @@ public class FactorialService
 {
     public static int Factorial(int n) {
         if(n==1 || n==0) {
-        return 1;
+            return 1;
         }
         return n * Factorial(n-1);
-
+    }
 
 }
